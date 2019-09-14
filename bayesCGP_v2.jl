59b38ad4116851e3977872999c4d10f59e3f28b3
SotaYoshida <52435497+SotaYoshida@users.noreply.github.com>
2019-09-14 11:50:48 +0900

    Add files via upload

--- bayesCGP_v2.jl
+++ bayesCGP_v2.jl
@@ -4,19 +4,21 @@ using SpecialFunctions
 using Printf
 using StatsBase
 using Base.Threads
+using LsqFit
+using BenchmarkTools
 include("./input.jl")
 include("./submodule/BayesGPsubmodule_v2.jl")
+#include("./submodule/KernelSelection.jl")
+#OMP_NUM_THREADS=4
+#ENV["PYTHON"]="python3.7"
+#ENV["PLOTS_DEFAULT_BACKEND"] = "PyPlot"
 
-
-ENV["PYTHON"]="python3.7"
-ENV["PLOTS_DEFAULT_BACKEND"] = "PyPlot"
 #using PyCall
 #using PyPlot
-#include("./ownplot.jl")
-#using .ownplot
 
 println("input file:$inpname")
 println("numN:$numN step:$mstep\n\n")
+println("parametric mean is used:",paramean)
 
 if occursin("N3LO",inpname); inttype="N3LO"
 elseif occursin("JISP16",inpname); inttype="JISP16"
@@ -24,14 +26,22 @@ elseif occursin("NNLOopt",inpname); inttype="NNLOopt"
 else; inttype="unknownint";end
 println("inttype $inttype")
 ## Initialize global variables
-Tsigma,tTheta,xtrain,ytrain,xprd,xun,yun,oxtrain,oytrain,iThetas,lt,lp,Mysigma,muy,mstd=readinput(inpname)
+Tsigma,tTheta,xtrain,ytrain,xprd,xun,yun,oxtrain,oytrain,iThetas,lt,lp,Mysigma,muy,mstd,pfit,Rtt,Rpt,Rpp=readinput(inpname)
+tKtp=zeros(Float64,lt,lp);tKpt=zeros(Float64,lp,lt)
 
-R=0.0;sigR=0.0
-@time R,sigR=detR(xtrain,ytrain,yun,mstep,Mysigma,muy,mstd)
-println("   |-> time for detR()")
+# R=0.0;sigR=0.0
+# Kernels=["logMatern","logMat32"]
+# qT=5.0;qY=5.0;qYfac = 1.0
+# KernelSelection(Kernels,xtrain,ytrain,yun,mstep,Mysigma,muy,mstd)
+# exit()
 
-#const R=0.5137080436573149; const sigR=0.1100703232765452
+# R=0.0;sigR=0.0
+# @time R,sigR=detR(xtrain,ytrain,yun,mstep,Mysigma,muy,mstd)
+# println("   |-> time for detR()")
+# #exit()
+
+R=(ytrain[lt-1]-ytrain[lt])/(ytrain[lt-2]-ytrain[lt-1])
+sigR = 0.1 * R
 @time Em,Ev=main(xtrain,ytrain,yun,mstep,numN,Mysigma,muy,mstd)
 println("   |-> time for main()")
-
 
